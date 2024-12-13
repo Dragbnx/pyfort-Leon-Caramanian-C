4d460d25fe071929b22106be6349753d20418022
A1NZ <159454904+Dragbnx@users.noreply.github.com>
2024-12-13 10:53:07 +0100

    changement place fct

--- epreuves_mathematiques.py
+++ epreuves_mathematiques.py
@@ -1,5 +1,30 @@
-from fonctions_utiles import factorielle, est_premier
 import random
+
+def factorielle(n):
+    """int -> int
+    renvoie la valeur de n! la fonction est code de maniere recursive
+    """
+    if n == 0: #cas de base
+        return 1
+    else: #itération pour les n-1
+        return n * factorielle(n-1)
+
+def est_premier(n):
+    """ int -> bool
+    renvoie si n est un nombre premier
+    """
+    for i in range(1, n): #boucle pour verifier chaque valeurs entre 1 et lui-même
+        if n % i == 0:
+            return False
+    return True
+
+def premier_plus_proche(n):
+    """ int -> int
+    renvoie le premier nombre premier supérieur ou égal à n
+    """
+    while not est_premier(n): #iteration tant que n n'est pas premier
+        n += 1
+    return n
 
 def epreuve_math_factorielle():
     """ None -> bool
--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -1,29 +1,3 @@
-def factorielle(n):
-    """int -> int
-    renvoie la valeur de n! la fonction est code de maniere recursive
-    """
-    if n == 0: #cas de base
-        return 1
-    else: #itération pour les n-1
-        return n * factorielle(n-1)
-
-def est_premier(n):
-    """ int -> bool
-    renvoie si n est un nombre premier
-    """
-    for i in range(1, n): #boucle pour verifier chaque valeurs entre 1 et lui-même
-        if n % i == 0:
-            return False
-    return True
-
-def premier_plus_proche(n):
-    """ int -> int
-    renvoie le premier nombre premier supérieur ou égal à n
-    """
-    while not est_premier(n): #iteration tant que n n'est pas premier
-        n += 1
-    return n
-
 def verification_saisie_entier(saisie):
     """ str -> bool
     renvoie True si la saisie est un entier, False sinon
