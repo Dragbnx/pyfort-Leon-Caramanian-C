e42dad229e30716a356e3abe010450ed34e643a5
A1NZ <159454904+Dragbnx@users.noreply.github.com>
2024-12-13 10:49:51 +0100

    implementation de verification_saisie_entier

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -24,3 +24,11 @@ def premier_plus_proche(n):
         n += 1
     return n
 
+def verification_saisie_entier(saisie):
+    """ str -> bool
+    renvoie True si la saisie est un entier, False sinon
+    """
+    for elm in saisie:
+        if not ord('0') <= ord(elm) <= ord('9'):
+            return False
+    return True
